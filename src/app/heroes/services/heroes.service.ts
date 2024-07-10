680f2d76159ad40552d89de1e38dbf9a142dcd4e
Juan Gomez <gomezsandovalpablo@gmail.com>
2024-07-10 15:33:07 -0700

    search heroes page / autocomplete

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -23,4 +23,10 @@ export class HeroesService {
       )
   }
 
+  getSuggestions(query: string): Observable<Hero[]> {
+     return this.http.get<Hero[]>( `${this.baseUrl}/heroes?q=${query}&_limit=6`);
+  }
+
+
+
 }
