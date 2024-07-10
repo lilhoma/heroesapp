f0bd387b3fbdf604357c669ef2fc4f4821ac2e06
Juan Gomez <gomezsandovalpablo@gmail.com>
2024-07-10 12:29:03 -0700

    hero page data

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { delay, switchMap } from 'rxjs';
 import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
@@ -18,6 +18,7 @@ export class HeroPageComponent implements OnInit {
   ngOnInit(): void {
     this.activatedRoute.params
       .pipe(
+        delay(1000),
         switchMap(({ id }) => this.heroService.getHeroById(id))
       ).subscribe(hero => {
         if (!hero) return this.router.navigate(['/heroes/list']);
@@ -27,5 +28,9 @@ export class HeroPageComponent implements OnInit {
 
         return;
       });
+  }
+
+  goBack(): void {
+    this.router.navigateByUrl('heroes/list');
   }
 }
